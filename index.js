6c1851aed8aedc5700b424a7ce1add88819941d4
Densyakun <densyakun1128@gmail.com>
2021-10-15 16:12:03 +0000

    JSON切り替えボタンによるCSS遷移で地図の位置を修正するように変更

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Map init
 var map = L.map('map').fitWorld();
+function invalidateSize() {
+    map.invalidateSize();
+}
+document.getElementById('collapseJson').addEventListener('shown.bs.collapse', invalidateSize);
+document.getElementById('collapseJson').addEventListener('hidden.bs.collapse', invalidateSize);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -41,7 +46,7 @@ info.onAdd = function (map) {
 };
 
 info.update = function (props) {
-    this._div.innerHTML = '<button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">JSON</button>';
+    this._div.innerHTML = '<button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseJson" aria-expanded="false" aria-controls="collapseJson">JSON</button>';
 };
 
 info.addTo(map);
