37b344f77a46e2fecbbe0765f7fd411c4a61a1c9
Densyakun <densyakun1128@gmail.com>
2021-10-15 05:41:18 +0000

    Add JSON editor modal

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ var map = L.map('map').fitWorld();
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+// JSON
+const text = document.querySelector("#jsonTextarea");
+var myLayer = L.geoJSON().addTo(map);
+
+function update(str) {
+    myLayer.clearLayers();
+    myLayer.addData(JSON.parse(str));
+}
+
+function updateValue(e) {
+    update(e.srcElement.value);
+}
+text.addEventListener('change', updateValue);
+update(text.value);
+
+// Copy to clipboard
+document.querySelector("#copy").addEventListener("click", function () {
+    text.select();
+    document.execCommand("copy");
+});
